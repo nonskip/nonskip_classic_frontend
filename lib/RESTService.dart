3fbb555f990478d5bfba1f0e5a8bebd296ca2cd7
Eu-Bin KIM <tlrndk123@gmail.com>
2023-04-09 09:50:54 +0900

    log 삭제. fastapi에 배포된 엔드포인트 적용

--- lib/RESTService.dart
+++ lib/RESTService.dart
@@ -9,7 +9,7 @@ class RESTService {
 
   static Future<OpenAIDialogue> chat(OpenAIDialogue dialogue) async {
     final response = await http.post(
-      Uri.parse('http://127.0.0.1:8000/chat'),
+      Uri.parse('https://nonskip-classic-api.vercel.app/chat'),
       headers: <String, String>{
         'Content-Type': 'application/json; charset=UTF-8',
       },
@@ -26,7 +26,7 @@ class RESTService {
 
   static Future<OpenAIDialogue> explain(String context, String selectedText) async {
     final response = await http.get(
-      Uri.parse('http://127.0.0.1:8000/explain?context=$context&s_text=$selectedText')
+      Uri.parse('https://nonskip-classic-api.vercel.app/explain?context=$context&s_text=$selectedText')
     );
     if (response.statusCode == 200) {
       // then parse the JSON.
@@ -40,7 +40,7 @@ class RESTService {
 
   static Future<OpenAIDialogue> reflect() async {
     final response = await http.get(
-      Uri.parse('http://127.0.0.1:8000/reflect')
+      Uri.parse('https://nonskip-classic-api.vercel.app/reflect')
     );
     if (response.statusCode == 200) {
       // then parse the JSON.
@@ -53,7 +53,7 @@ class RESTService {
   }
 
   static Future<OpenAIDialogue> coffeeChat() async {
-      final response = await http.get(Uri.parse('http://127.0.0.1:8000/coffeechat'));
+      final response = await http.get(Uri.parse('https://nonskip-classic-api.vercel.app/coffeechat'));
       if (response.statusCode == 200) {
         // then parse the JSON.
         return OpenAIDialogue.fromJson(jsonDecode(utf8.decode(response.bodyBytes)));
@@ -62,24 +62,5 @@ class RESTService {
         // then throw an exception.
         throw Exception('Failed to create Dialogue.');
       }
-  }
-
-  static void log(String context, String selectedText, OpenAIDialogue dialogue) async {
-    final response = await http.post(
-      Uri.parse('...'),
-      headers: <String, String>{
-        'Content-Type': 'application/json; charset=UTF-8',
-      },
-      body: jsonEncode(<String, dynamic>{
-        'context': context,
-        's_text': selectedText,
-        'dialogue': dialogue.toJson(),
-      })
-    );
-    if (response.statusCode != 200) {
-      // If the server did not return a 201 CREATED response,
-      // then throw an exception.
-      throw Exception('Failed to create Dialogue.');
-    }
   }
 }
